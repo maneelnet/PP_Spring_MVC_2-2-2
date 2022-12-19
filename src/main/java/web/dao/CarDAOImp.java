26b462dcc0c4b3396159d9e56501ddeb745dc4b6
maneelnet <maneelnet@gmail.com>
2022-12-19 14:55:38 +0900

    PP Spring MVC task 2.2.2

---
+++ src/main/java/web/dao/CarDAOImp.java
@@ -0,0 +1,42 @@
+package web.dao;
+
+import org.springframework.stereotype.Repository;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class CarDAOImp implements CarDAO {
+
+    private List<Car> carsList;
+
+    {
+        carsList = new ArrayList<>();
+
+        carsList.add(new Car("Nissan", "100 NX", 100.0));
+        carsList.add(new Car("Honda", "Accord", 160.0));
+        carsList.add(new Car("Audi", "A8", 200.0));
+        carsList.add(new Car("Suzuki", "Aerio", 180.0));
+        carsList.add(new Car("Toyota", "GT86", 250.0));
+    }
+
+    @Override
+    public List<Car> showAllCars() {
+        return carsList;
+    }
+
+    @Override
+    public List<Car> showCars(int count) {
+        if (count < 5) {
+            List<Car> secondCarsList = new ArrayList<>(carsList);
+            for (int i = carsList.size() - 1; i >= count; i--) {
+                secondCarsList.remove(i);
+            }
+            return secondCarsList;
+        } else {
+            return carsList;
+        }
+    }
+
+}
